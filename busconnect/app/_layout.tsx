31280cba832544ac636ad3969eeeccc347f0f1ee
mts-lucas <lmateus1067@outlook.com>
2025-05-30 17:03:26 -0300

    chore: refactor layout logic #3

--- busconnect/app/_layout.tsx
+++ busconnect/app/_layout.tsx
@@ -3,8 +3,8 @@ import { Slot, Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)/home" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
