f5fc9c24d02d0cc049ab38b3e4c1a9d5b73f8eb8
mts-lucas <lmateus1067@outlook.com>
2025-05-30 18:23:25 -0300

    style: change logo text for image #3

--- busconnect/components/Logo/styles.ts
+++ busconnect/components/Logo/styles.ts
@@ -1,14 +1,11 @@
 import { StyleSheet } from 'react-native';
-import { COLORS } from '../../constants/colors';
 
 export const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    marginBottom: 32,
   },
-  text: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: COLORS.yellowDark,
+  image: {
+    width: '100%',
+    borderRadius: 8,
   },
 });
