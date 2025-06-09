9f8856b64925a963784e8d3ce9eaa554dfd7c79e
CayoLopes <cayocesarlopes@gmail.com>
2025-06-08 23:35:27 -0300

    mocks: Dados alunos e motorista #5

---
+++ busconnect/mocks/data.ts
@@ -0,0 +1,28 @@
+
+export const viagens = [
+  {
+    diaDaViagem: "2023-10-15",
+    rota: "Jucurutu -> Caicó -> Jucurutu",
+    motorista: {
+      nome: "João Silva",
+      foto: "https://static1.purebreak.com.br/articles/0/10/11/60/@/413853-batalha-de-looks-agostinho-carrara-vote-580x0-2.jpg",
+      habilitacao: "D"
+    },
+    alunos: [
+      {
+        nome: "Maria Oliveira",
+        foto: "https://pipocasclub.com.br/wp-content/uploads/2021/05/sandy-cheeks-background-wallpaper-72703-75288-hd-wallpapers-scaled.jpg",
+        instituicao: "Escola A",
+        matricula: "2023001",
+        volta: true
+      },
+      {
+        nome: "Carlos Souza",
+        foto: "https://i.redd.it/l6qjnnam7nu81.jpg",
+        instituicao: "Escola A",
+        matricula: "2023002",
+        volta: false
+      }
+    ]
+  }
+];
