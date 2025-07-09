bd3d40b5f4473e3daca0da70aaa96f41d7afd7e3
CayoLopes <cayocesarlopes@gmail.com>
2025-07-09 15:37:52 -0300

    add: configurações adicionais do firebase #21

--- busconnect/firebaseConfig.ts
+++ busconnect/firebaseConfig.ts
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// @ts-ignore 
+// @ts-ignore
 import { initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import Constants from 'expo-constants'; //
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from 'firebase/storage';
+import Constants from 'expo-constants';
 
-// Your web app's Firebase configuration
-const firebaseApiKey = Constants.expoConfig?.extra?.firebaseApiKey as string | undefined;
-const firebaseAuthDomain = Constants.expoConfig?.extra?.firebaseAuthDomain as string | undefined;
-const firebaseProjectId = Constants.expoConfig?.extra?.firebaseProjectId as string | undefined;
-const firebaseStorageBucket = Constants.expoConfig?.extra?.firebaseStorageBucket as string | undefined;
-const firebaseMessagingSenderId = Constants.expoConfig?.extra?.firebaseMessagingSenderId as string | undefined;
-const firebaseAppId = Constants.expoConfig?.extra?.firebaseAppId as string | undefined;
+const firebaseApiKey = Constants.expoConfig?.extra?.firebaseApiKey;
+const firebaseAuthDomain = Constants.expoConfig?.extra?.firebaseAuthDomain;
+const firebaseProjectId = Constants.expoConfig?.extra?.firebaseProjectId;
+const firebaseStorageBucket = Constants.expoConfig?.extra?.firebaseStorageBucket;
+const firebaseMessagingSenderId = Constants.expoConfig?.extra?.firebaseMessagingSenderId;
+const firebaseAppId = Constants.expoConfig?.extra?.firebaseAppId;
 
 if (
   !firebaseApiKey ||
@@ -37,11 +34,9 @@ const firebaseConfig = {
   appId: firebaseAppId,
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = initializeAuth(app);
-export { app, db };
+export { app, db, storage };
